b08e537f07e9183060f21f1ff57c8a6897fe43fb
Frank Cline <frankmcline@gmail.com>
2023-12-08 19:16:37 -0900

    Finished day 4 part a

--- Day4/day4.py
+++ Day4/day4.py
@@ -7,7 +7,27 @@ class Day4(Day):
         Day.__init__(self, 4)
 
     def get_solution_a(self, input_data):
-        pass
+        points = 0
+        for card in input_data:
+            nums = card.split(':')[1].split(' | ')
+            winning_nums = nums[0].strip().split(' ')
+            drawn_nums = nums[1].strip().split(' ')
+            winners = set()
+            total_won = 0
+            for num in winning_nums:
+                if num == '':
+                    continue
+                winners.add(int(num))
+            for num in drawn_nums:
+                if num == '':
+                    continue
+                num = int(num)
+                if num in winners:
+                    total_won += 1
+            if total_won > 0:
+                points += pow(2, total_won - 1)
+        return points
+        
 
     def get_solution_b(self, input_data):
         pass
