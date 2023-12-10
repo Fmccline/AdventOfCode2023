dfbc600863842491fac23a0a849e2ee368ee8f44
Frank Cline <frankmcline@gmail.com>
2023-12-09 19:00:01 -0900

    Refactored to show how solution a and b are practically the same

--- Day6/day6.py
+++ Day6/day6.py
@@ -6,6 +6,7 @@ class Day6(Day):
     def __init__(self):
         Day.__init__(self, 6)
 
+
     def get_records_a(self, input_data):
         times = []
         for time in input_data[0].split(':')[1].strip().split(' '):
@@ -17,40 +18,43 @@ class Day6(Day):
                 distances.append(int(distance))
         return list(zip(times, distances))
 
-    def get_solution_a(self, input_data):
-        records = self.get_records_a(input_data)
-        power_win = 1
-        for record in records:
-            '''
-            t = time held = velocity
-            T = time of race
-            d = distance
-            d = t*(T - t)
-            '''
-            ways_to_win = 0
-            T = record[0]
-            D = record[1]
-            for t in range(T - 1):
-                d = t*(T - t)
-                if d > D:
-                    ways_to_win += 1
-            power_win *= ways_to_win
-        return power_win
 
-    def get_records_b(self, input_data):
+    def get_record_b(self, input_data):
         time = int(input_data[0].split(':')[1].replace(' ', ''))
         distance = int(input_data[1].split(':')[1].replace(' ', ''))
-        return time, distance
+        return (time, distance)
 
-    def get_solution_b(self, input_data):
+
+    def get_ways_to_win(self, record):
+        '''
+        T = time of race
+        D = record distance
+        t = time held = velocity
+        d = distance
+        d = t*(T - t)
+        '''
         ways_to_win = 0
-        T, D = self.get_records_b(input_data)
+        T = record[0]
+        D = record[1]
         for t in range(T - 1):
             d = t*(T - t)
             if d > D:
                 ways_to_win += 1
         return ways_to_win
-        
+
+
+    def get_solution_a(self, input_data):
+        records = self.get_records_a(input_data)
+        power_win = 1
+        for record in records:
+            power_win *= self.get_ways_to_win(record)
+        return power_win
+
+
+    def get_solution_b(self, input_data):
+        return self.get_ways_to_win(self.get_record_b(input_data))
+    
+
     def get_expected_a(self):
         return 288
 
