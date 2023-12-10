00bbe91afded3f99bfff46b5aeaa59d28dcbf29d
Frank Cline <frankmcline@gmail.com>
2023-12-09 18:50:30 -0900

    Finished day 6 part a

--- Day6/day6.py
+++ Day6/day6.py
@@ -6,8 +6,36 @@ class Day6(Day):
     def __init__(self):
         Day.__init__(self, 6)
 
+    def get_records(self, input_data):
+        times = []
+        for time in input_data[0].split(':')[1].strip().split(' '):
+            if time != '':
+                times.append(int(time))
+        distances = []
+        for distance in input_data[1].split(':')[1].strip().split(' '):
+            if distance != '':
+                distances.append(int(distance))
+        return list(zip(times, distances))
+
     def get_solution_a(self, input_data):
-        pass
+        records = self.get_records(input_data)
+        power_win = 1
+        for record in records:
+            '''
+            t = time held = velocity
+            T = time of race
+            d = distance
+            d = t*(T - t)
+            '''
+            ways_to_win = 0
+            T = record[0]
+            D = record[1]
+            for t in range(T - 1):
+                d = t*(T - t)
+                if d > D:
+                    ways_to_win += 1
+            power_win *= ways_to_win
+        return power_win
 
     def get_solution_b(self, input_data):
         pass
