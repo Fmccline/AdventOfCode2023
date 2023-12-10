ad1fed875539975d22ecdf322ece44c1550b0956
Frank Cline <frankmcline@gmail.com>
2023-12-09 21:04:25 -0900

    Finished day 7 part b

--- Day7/day7.py
+++ Day7/day7.py
@@ -21,26 +21,79 @@ class Day7(Day):
                 cards[card] = 0
             cards[card] += 1
         # 5 of a kind
-        if len(cards.keys()) == 1:
+        if 5 in cards.values():
             return 6
-        # 4 of a kind, full house
-        elif len(cards.keys()) == 2:
-            if 4 in cards.values():
-                return 5
-            else:
+        # 4 of a kind
+        elif 4 in cards.values():
+            return 5
+        # full house, 3 of a kind
+        elif 3 in cards.values():
+            if 2 in cards.values():
                 return 4
-        # 3 of a kind, two pair
-        elif len(cards.keys()) == 3:
-            if 3 in cards.values():
-                return 3
             else:
-                return 2
-        # one pair
-        elif len(cards.keys()) == 4:
-            return 1
+                return 3
+        # 2 pair, 1 pair
+        elif 2 in cards.values():
+            pairs = 0
+            for value in cards.values():
+                if value == 2:
+                    pairs += 1
+            return pairs # 2 or 1
         # must be high card
         else:
             return 0
+
+    def get_hand_type_with_wild(self, hand):
+        cards = {}
+        wilds = 0
+        for card in hand:
+            if card == '1':
+                wilds += 1
+
+        hand_type = self.get_hand_type(hand.replace('1',''))
+        best_hands = {
+            1: {
+                # 5 -> 6 : 4 kind -> 5 kind
+                # 4 -> 5 : full house -> 4 of a kind
+                # 3 -> 5 : 3 kind -> 4 kind
+                # 2 -> 4 : 2 pair -> full house
+                # 1 -> 3 : 1 pair -> 3 kind
+                # 0 -> 1 : high -> 1 pair
+                5 : 6,
+                4 : 5,
+                3 : 5,
+                2 : 4,
+                1 : 3,
+                0 : 1
+            }, 
+            2: {
+                # 3 -> 6 : 3 kind -> 5 kind
+                # 1 -> 4 : 1 pair -> 4 kind
+                # 0 -> 3 : high -> 3 kind
+                3 : 6,
+                1 : 5,
+                0 : 3
+            },
+            3: {
+                # 1 -> 6 : 1 pair -> 5 kind
+                # 0 -> 5 : high -> 4 kind
+                1 : 6,
+                0 : 5,
+            },
+            4: {
+                0 : 6
+            },
+            5: {
+                0 : 6
+            }
+        }
+        if wilds == 0:
+            return hand_type
+        else:
+            return best_hands[wilds][hand_type]
+            
+        return hand_type
+        
         
     def get_hands(self, input_data):
         hands = []
@@ -91,10 +144,33 @@ class Day7(Day):
 
 
     def get_solution_b(self, input_data):
-        pass
-        
+        hand_types = {}
+        for i in range(7):
+            hand_types[i] = []
+        hands = self.get_hands(input_data)
+        for idx in range(len(hands)):
+            hand = hands[idx][0]
+            hand = hand.replace('W', '1')
+            hands[idx] = (hand, hands[idx][1])
+        for hand in hands:
+            hand_type = self.get_hand_type_with_wild(hand[0])
+            hand_types[hand_type].append(hand)
+        ranked_hands = []
+        for hand_type in range(7):
+            hands = hand_types[hand_type]
+            if not hands:
+                continue
+            hands.sort(key=lambda hand: hand[0])
+            ranked_hands.extend(hands)
+        winnings = 0
+        for rank in range(len(ranked_hands)):
+            hand = ranked_hands[rank]
+            winnings += hand[1]*(rank+1)
+        return winnings        
+
+
     def get_expected_a(self):
         return 6440
 
     def get_expected_b(self):
-        pass
+        return 5905
