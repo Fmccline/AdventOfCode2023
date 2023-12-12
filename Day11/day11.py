926e27e5dea4082a73ae50732269e4201030d782
Frank Cline <frankmcline@gmail.com>
2023-12-11 21:54:50 -0900

    Finished day 11 part b

--- Day11/day11.py
+++ Day11/day11.py
@@ -5,17 +5,26 @@ class Day11(Day):
     def __init__(self):
         Day.__init__(self, 11)
 
-    def get_solution_a(self, input_data):
-        # find empty rows and expand
+    def get_grid(self, input_data):
         grid = []
         for line in input_data:
-            line = line.replace('\n', '')
-            if '#' not in line:
-                grid.append(line)
-            grid.append(line)
+            row = []
+            for space in line.replace('\n', ''):
+                row.append(space)
+            grid.append(row)
+        return grid
 
-        # find empty columns and expand
-        empty_cols = set()
+    def get_galaxies(self, grid):
+        galaxies = []
+        for row in range(len(grid)):
+            for col in range(len(grid[0])):
+                if grid[row][col] == '#':
+                    galaxies.append((row, col))
+                    grid[row][col] = str(len(galaxies))
+        return galaxies
+
+    def get_empty_cols(self, grid):
+        empty_cols = []
         for col in range(len(grid[0])):
             is_empty = True
             for row in grid:
@@ -23,45 +32,59 @@ class Day11(Day):
                     is_empty = False
                     break
             if is_empty:
-                empty_cols.add(col)
+                empty_cols.append(True)
+            else:
+                empty_cols.append(False)
+        return empty_cols
 
-        new_grid = []
+    def get_empty_rows(self, grid):
+        empty_rows = []
         for row in grid:
-            new_row = []
-            for col in range(len(row)):
-                space = row[col]
-                if col in empty_cols:
-                    new_row.append('.')
-                new_row.append(space)
-            new_grid.append(new_row)
+            if '#' in row:
+                empty_rows.append(False)
+            else:
+                empty_rows.append(True)
+        return empty_rows
 
-        grid = new_grid
+    def get_distance(self, p1, p2, empty_list, distance):
+        d = 0
+        start = min(p1, p2)
+        end = max(p1, p2)
+        for p in range(start, end):
+            if empty_list[p] is True:
+                d += distance
+            else:
+                d += 1
+        return d
 
-        # find galaxies in grid
-        galaxies = []
-        for row in range(len(grid)):
-            for col in range(len(grid[0])):
-                if grid[row][col] == '#':
-                    galaxies.append((row, col))
-                    grid[row][col] = str(len(galaxies))
+    def get_solution(self, input_data, empty_distance):
+        grid = self.get_grid(input_data)
+        empty_cols = self.get_empty_cols(grid)
+        empty_rows = self.get_empty_rows(grid)
+        galaxies = self.get_galaxies(grid)
         
-        # go through galaxies and find shortest path between each pair
-        shortest_paths = []
+        smallest_distances = 0
         for galaxy_idx in range(len(galaxies) - 1):
             galaxy = galaxies[galaxy_idx]
             for next_idx in range(galaxy_idx + 1, len(galaxies)):
                 next_galaxy = galaxies[next_idx]
-                x_dist = abs(galaxy[0] - next_galaxy[0]) 
-                y_dist = abs(galaxy[1] - next_galaxy[1])
-                shortest_paths.append(x_dist + y_dist)
-
-        return sum(shortest_paths)
+                y1 = galaxy[0]
+                x1 = galaxy[1]
+                y2 = next_galaxy[0]
+                x2 = next_galaxy[1]
+                y_dist = self.get_distance(y1, y2, empty_rows, empty_distance)
+                x_dist = self.get_distance(x1, x2, empty_cols, empty_distance)
+                smallest_distances += x_dist + y_dist
+        return smallest_distances
+    
+    def get_solution_a(self, input_data):
+        return self.get_solution(input_data, 2)
 
     def get_solution_b(self, input_data):
-        pass
+        return self.get_solution(input_data, 1000000)
         
     def get_expected_a(self):
         return 374
 
     def get_expected_b(self):
-        pass
+        return 82000210
